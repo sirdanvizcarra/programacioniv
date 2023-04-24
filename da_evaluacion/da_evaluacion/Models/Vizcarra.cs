88d5ddd0616570e5de6884d7c9ba78153582b2e3
laboratorio <laboratorio@UPOAULA10203>
2023-04-24 08:20:09 -0500

    modelo

---
+++ da_evaluacion/da_evaluacion/Models/Vizcarra.cs
@@ -0,0 +1,16 @@
+﻿namespace da_evaluacion.Models
+{
+    public class Vizcarra
+    {
+
+        public int daId { get; set; }
+        public string? dvName { get; set; }
+        public DateTime dvNacimiento { get; set; }
+        public int dvEdad { get; set; }
+        public decimal dvEstatura { get; set; }
+        public bool dvActivo { get; set; }
+        
+
+
+    }
+}
