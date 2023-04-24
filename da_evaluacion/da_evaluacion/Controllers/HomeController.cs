4a2361fe839005fec5a26bcd16896ef6caabe3f9
laboratorio <laboratorio@UPOAULA10203>
2023-04-24 07:43:05 -0500

    creation

---
+++ da_evaluacion/da_evaluacion/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using da_evaluacion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace da_evaluacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
