47865e601a35423d0ee21c0154b62fd7b79a69ae
Cisco <cisco@udla-ec.int>
2023-05-29 08:29:51 -0500

    Personalización DV

--- DV_Apuntes/DV_Apuntes/AboutPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace DV_Apuntes;
-
-public partial class AboutPage : ContentPage
-{
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
-}
---
+++ DV_Apuntes/DV_Apuntes/DV_AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace DV_Apuntes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+
+	}
+
+    private async void DV_LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
